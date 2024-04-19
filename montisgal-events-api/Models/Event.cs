50896d976c49c1c007f29f43b01db1e641df8d50
almonteagudor <55769333+almonteagudor@users.noreply.github.com>
2024-04-19 18:07:49 +0200

    Event Model added

---
+++ montisgal-events-api/Models/Event.cs
@@ -0,0 +1,10 @@
+﻿namespace montisgal_events_api.Models;
+
+public class Event(Guid id, string name, string? description, DateTime startDate, DateTime? endDate)
+{
+    public Guid Id { get; set; } = id;
+    public string Name { get; set; } = name;
+    public string? Description { get; set; } = description;
+    public DateTime StartDate { get; set; } = startDate;
+    public DateTime? EndDate { get; set; } = endDate;
+}
