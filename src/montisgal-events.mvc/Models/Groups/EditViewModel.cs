cb2eb067b82d70d160b83ecbd8aef741d190f11b
almonteagudor <55769333+almonteagudor@users.noreply.github.com>
2024-06-08 01:48:49 +0200

    Symfony 7.1

--- src/montisgal-events.mvc/Models/Groups/EditViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace montisgal_events.mvc.Models.Group;
-
-public class EditViewModel
-{
-    public Guid Id { get; }
-    public string? Name { get; set; }
-    public string? Description { get; set; }
-    public bool IsPublic { get; set; }
-}
--- src/montisgal-events.mvc/Models/Groups/IndexViewModel.cs
+++
@@ -1,6 +0,0 @@
-namespace montisgal_events.mvc.Models.Groups;
-
-public class IndexViewModel(List<domain.Groups.Group> groups)
-{
-    public List<domain.Groups.Group> Groups { get; set; } = groups;
-}
